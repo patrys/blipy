896b28aa805ea645c81ce173369a7f1be13cfdc2
Patryk Zawadzki <patrys@pld-linux.org>
2008-02-08 16:25:29 +0000

    - initial commit
    git-svn-id: https://blipy.googlecode.com/svn/trunk@2 f852e4dd-a745-0410-957d-013bef1c1321

---
+++ blipy/__init__.py
@@ -0,0 +1,261 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+ blipoc - Blip.pl On Console
+ This is part of blipoc - python-blipoc (or in short: pblipoc)
+
+ $Id: api.py 41 2008-01-31 11:39:02Z patrys $
+ 
+ author: Cezary Statkiewicz (cezio [at] thelirium.net), Patryk Zawadzki <patrys@pld-linux.org>
+ website: http://code.google.com/p/blipoc/
+
+ API: http://www.blip.pl/api-0.02.html
+ version: 0.02
+ 
+ To use it:
+ import pblipoc.api
+
+"""
+
+# some globals
+
+UPDATE_BODY_LIMIT = 160
+
+import types
+import datetime
+from core import BaseApiObject, ApiException, BlipocInputError, Request
+cached = {}
+
+def propertize(name, cls):
+	def proxied_property(self):
+		if not cached.has_key(name):
+			cached[name] = {}
+		if hasattr(self, '%s_path' % name):
+			uri = getattr(self, '%s_path' % name)
+			if not cached[name].has_key(uri):
+				c = globals()[cls]
+				cached[name][uri] = c.get_by_uri(self.account, uri)
+			return cached[name][uri]
+		else:
+			return None
+	return proxied_property
+
+class Account(object):
+	credentials = None
+
+	def set_credentials(self, username, password):
+		"""
+		set username and password 
+		"""
+		self.credentials = (username, password,)
+
+class Movie(BaseApiObject):
+	__fields__ = {'id': int,
+					'url': unicode}
+
+class Picture(BaseApiObject):
+	__fields__ = {'id': int,
+					'url': unicode,
+					'update_path': unicode}
+	update = property(propertize('update', 'Update'), None)
+
+	@staticmethod
+	def list(account, update_id = None):
+		if update_id:
+			return Picture.get_list_by_uri(account, '/pictures/%s/all_since' % update_id)
+		else:
+			return Picture.get_list_by_uri(account, '/pictures/all')
+
+class Recording(BaseApiObject):
+	__fields__ = {'id': int,
+					'url': unicode,
+					'update_path': unicode}
+	update = property(propertize('update', 'Update'), None)
+
+class Shortlink(BaseApiObject):
+	__fields__ = {'id': int,
+					'created_at': datetime.datetime,
+					'hit_count': int,
+					'original_link': unicode,
+					'shortcode': unicode}
+
+	@staticmethod
+	def list(account, update_id = None):
+		if update_id:
+			return Shortlink.get_list_by_uri(account, '/shortlinks/%s/all_since' % update_id)
+		else:
+			return Shortlink.get_list_by_uri(account, '/shortlinks/all')
+
+class Transport(BaseApiObject):
+	__fields__ = {'id': int,
+					'name': unicode}
+
+class Update(BaseApiObject):
+	__fields__ = { 'id': int,
+					'body': unicode,
+					'type': unicode,
+					'user_path': unicode,
+					'recipient_path': unicode,
+					'created_at': datetime.datetime,
+					'transport': Transport,
+					'pictures_path': unicode,
+					'recording_path': unicode,
+					'movie_path': unicode}
+	recipient = property(propertize('recipient', 'User'), None)
+	user = property(propertize('user', 'User'), None)
+	recording = property(propertize('recording', 'Recording'), None)
+	movie = property(propertize('movie', 'Movie'), None)
+
+	def get_pictures(self):
+		if hasattr(self, 'pictures_path'):
+			return Picture.get_list_by_uri(self.account, self.pictures_path)
+		else:
+			return []
+	pictures = property(get_pictures, None)
+
+	@staticmethod
+	def dashboard(account, update_id = None):
+		"""
+		retrieves user's dashboard.
+		if update_id provided, it will return changes made after update_id
+		"""
+		if update_id:
+			return Update.get_list_by_uri(account, '/dashboard/since/%s' % update_id)
+		else:
+			return Update.get_list_by_uri(account, '/dashboard')
+
+	@staticmethod
+	def list(account, update_id = None):
+		"""
+		retrieves user's statuses.
+		if update_id provided, it will return changes made after update_id
+		"""
+		if update_id:
+			return Update.get_list_by_uri(account, '/updates/%s/since' % update_id)
+		else:
+			return Update.get_list_by_uri(account, '/updates')
+
+	@classmethod
+	def get(cls, account, update_id):
+		"""
+		get specified status
+		"""
+		return cls.get_by_uri(account, '/updates/%s' % update_id)
+
+	@staticmethod
+	def create(account, status):
+		"""
+		set new status
+		"""
+		if len(status)> UPDATE_BODY_LIMIT:
+			raise BlipocInputError('status jest dłuższy niż dopuszczalna wielkość: (%s > %s)'%(len(status), UPDATE_BODY_LIMIT) )
+		r = Request(account.credentials, '/updates', 'POST', {'update[body]': status})
+		return r.do_request()
+
+class DirectedMessage(Update):
+	@staticmethod
+	def list(account, update_id = None):
+		"""
+		retrieves user's directed messages.
+		if update_id provided, it will return changes made after update_id
+		"""
+		if update_id:
+			return DirectedMessage.get_list_by_uri(account, '/directed_messages/%s/since' % update_id)
+		else:
+			return DirectedMessage.get_list_by_uri(account, '/directed_messages')
+
+	@classmethod
+	def get(cls, account, update_id):
+		"""
+		get specified status
+		"""
+		return cls.get_by_uri(account, '/directed_messages/%s' % update_id)
+
+	@staticmethod
+	def create(account, status, recipient):
+		"""
+		create private message
+		"""
+		if len(status)> UPDATE_BODY_LIMIT:
+			raise BlipocInputError('status jest dłuższy niż dopuszczalna wielkość: (%s > %s)'%(len(status), UPDATE_BODY_LIMIT) )
+		r = Request(account.credentials, '/directed_messages', 'POST', {'directed_message[body]': status, 'directed_message[recipient]': recipient})
+		return r.do_request()
+
+class Status(Update):
+	@staticmethod
+	def list(account, update_id = None):
+		"""
+		retrieves user's status messages.
+		if update_id provided, it will return changes made after update_id
+		"""
+		if update_id:
+			return Status.get_list_by_uri(account, '/statuses/%s/all_since' % update_id)
+		else:
+			return Status.get_list_by_uri(account, '/statuses/all')
+
+	@classmethod
+	def get(cls, account, update_id):
+		"""
+		get specified status
+		"""
+		return cls.get_by_uri(account, '/statuses/%s' % update_id)
+
+	@staticmethod
+	def create(account, status):
+		"""
+		create status message
+		"""
+		if len(status)> UPDATE_BODY_LIMIT:
+			raise BlipocInputError('status jest dłuższy niż dopuszczalna wielkość: (%s > %s)'%(len(status), UPDATE_BODY_LIMIT) )
+		r = Request(account.credentials, '/statuses', 'POST', {'status[body]': status})
+		return r.do_request()
+
+class Bliposphere(BaseApiObject):
+	@staticmethod
+	def list(account):
+		return Status.get_list_by_uri(account, '/bliposphere')
+
+class Avatar(BaseApiObject):
+	__fields__ = { 'id': int,
+					'url_15': unicode,
+					'url_30': unicode,
+					'url_50': unicode,
+					'url_90': unicode,
+					'url_120': unicode,
+					'url': unicode}
+
+class Background(BaseApiObject):
+	__fields__ = { 'id': int,
+					'url': unicode}
+
+class User(BaseApiObject):
+	__fields__ = {'id': int,
+				'login': unicode,
+				'avatar_path': unicode,
+				'background_path': unicode,
+				'current_status_path': unicode}
+	current_status = property(propertize('current_status', 'Status'), None)
+	avatar = property(propertize('avatar', 'Avatar'), None)
+	background = property(propertize('background', 'Background'), None)
+
+	@staticmethod
+	def friends(account):
+		"""
+		get list of user's friends
+		"""
+		return User.get_list_by_uri(account, '/friends/')
+
+	@classmethod
+	def get(cls, account, user = None):
+		"""
+		get user data for user
+		if user is not provided, api will return current user
+		"""
+		if user:
+			return cls.get_by_uri(account, '/users/%s' % user)
+		else:
+			return cls.get_by_uri(account, '/users/')
+
+#
+# $Id: api.py 41 2008-01-31 11:39:02Z patrys $
+#
---
+++ blipy/core.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# blipoc - Blip On Console
+#
+# author: Cezary Statkiewicz (cezio [at] thelirium.net), Patryk Zawadzki <patrys@pld-linux.org>
+# website: http://code.google.com/p/blipoc/
+#
+# version: $Id: core.py 38 2008-01-28 16:18:29Z patrys $
+
+API_VERSION = 0.02
+URL = 'http://api.blip.pl'
+USER_AGENT = 'blipoc - Blip on console'
+DEBUG = False # to be False in stable version
+
+import datetime, time, urllib2, urllib, base64
+import simplejson
+from pprint import pprint
+
+class ApiException(Exception):
+	pass
+
+class BlipocInputError(ApiException):
+	pass
+
+class BaseApiObject(object):
+	"""
+	base blip api class for containers
+	
+	__fields__ - dictionary {field: field_type_class_or_type} with attributes of this type
+	__values__ - dictionary with object values
+	
+	get_json() - method for getting json string for class instance
+	
+	parse_json(data, raw_json = False) - method for setting attributes from json data, if raw_json, then it will be parsed by simplejson.loads
+
+	"""
+	__fields__ = {}
+
+	def __init__(self, account = None, *args, **kwargs):
+		self.__values__ = None
+		self.account = account
+		if args:
+			try:
+				self.parse_json(args[0])
+			except Exception, e:
+				if DEBUG:
+					print args[0]
+					print '%s'%e
+
+	@classmethod
+	def get_by_uri(cls, account, uri):
+		"""
+		get specified status
+		"""
+		r = Request(account.credentials, uri, 'GET', None)
+		data = r.request_json()
+		return cls(account, data)
+
+	@classmethod
+	def get_list_by_uri(cls, account, uri):
+		"""
+		get specified status
+		"""
+		r = Request(account.credentials, uri, 'GET', None)
+		data = r.request_json()
+		res = []
+		for i in data:
+			res.append(cls(account, i))
+		return res
+
+	def get_json(self):
+		data = {}
+		for f in self.__fields__:
+			# getting field value
+			try:
+				field = getattr( self, f)
+			except AttributeError:
+				# maybe if it's required field it should raise an exception?
+				break
+			if isinstance(field, BaseApiObject):
+				data[f] = field.get_json()
+			else:
+				data[f] = field
+		return simplejson.dumps(data)
+
+	def parse_json(self, data, raw_json = False):
+		if raw_json:
+			data = simplejson.loads(data)
+		data = self._parse_json(data)
+		for f in data:
+			if DEBUG:
+				print 'setting self.%s = %s'%( f, data[f])
+			setattr(self, f, data[f])
+		self.__values__ = data
+
+	def _parse_json(self, data):
+		d = {}
+		if not isinstance(data, dict):
+			raise ApiException('Nieprawidłowe dane - spodziewano się słownika, otrzymano %s' % type(data))
+		for f in self.__fields__:
+			try:
+				field = data[f]
+			except KeyError:
+				if DEBUG:
+					print 'missing key', f
+				continue
+			try:
+				if self.__fields__[f] is datetime.datetime:
+					value = datetime.datetime.strptime(field, '%Y-%m-%d %H:%M:%S')
+				elif issubclass (self.__fields__[f], BaseApiObject):
+					value = self.__fields__[f](self.account, field)
+				else:
+					value = self.__fields__[f](field)
+			except Exception, e:
+				raise ApiException('Błąd walidacji typu dla obiektu %s: atrybut %s %s' % ( self.__class__.__name__, f, e))
+			d[f] = value
+		return d
+
+	def _print(self):
+		"""
+		TODO: rewite!!!
+		
+		formated object representation
+		
+Class1:
+	attr1: value
+	class_attr:
+	
+		Class2:
+			attr2: value
+			attr3: value
+		
+		"""
+		out = ''
+		for f in self.__fields__:
+			try:
+				if isinstance(getattr(self, f), BaseApiObject):
+					tmp = getattr(self, f)._print().split()
+					out +='\t%s\n:'%f
+					out +='\n\t'.join([ '\t%s'%l for l in  tmp])
+				else:
+					out += '\t%s:%s\n'%(f, getattr(self, f))
+			except AttributeError:
+				continue
+		#####
+		#out = '\n'.join( [ '\t%s : %s'%( k, getattr(self, k, '') )
+		#					for k in self.__fields__  ])
+		
+		out = '%s: \n%s\n'%(self.__class__.__name__, out)
+		return out
+		
+	__str__ = _print
+	__repr__ = _print
+
+class Request(object):
+	"""
+	request object for api.blip.pl
+	"""
+	def __init__(self, credentials, url, method, data):
+		self.credentials = credentials
+		self.url = '%s%s' % (URL, url)
+		self.method = method
+		if type(data) == dict:
+			data = urllib.urlencode(data)
+		self.data = data
+		self._debug = DEBUG
+		
+	def do_request(self):
+		if self._debug:
+			self.__print_debug('Requesting url: %s'%self.url)
+		request = urllib2.Request(self.url)
+		if self.credentials:
+			request.add_header('Authorization', 
+								'Basic %s'%( base64.b64encode('%s:%s'%self.credentials )))
+		request.add_header('User-Agent', USER_AGENT)
+		request.add_header('X-blip-api', '%s'%API_VERSION)
+		request.add_header('Accept', 'application/json')
+		request.add_header('Pragma', 'no-cache')
+		
+		if self.data:
+			request.add_data(self.data)
+			request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+		try:
+			response = urllib2.urlopen(request)
+		except urllib2.HTTPError, e:
+			# 201 - Created:
+			
+			data = e.read()
+			if e.code == 201:
+				self.__print_debug(data)
+				return True
+			self.__print_debug('Requested url: %s'%self.url)
+			self.__print_debug('error!!!!\nHTTP %s\n%s\n\n%s'%(e.code, e.headers, data ))
+			return data
+		data = response.read()
+		if self._debug:
+			self.__print_debug(data)
+		return data
+
+	def request_json(self):
+		return simplejson.loads(self.do_request())
+
+	def __print_debug(self, data):
+		print '>>> pblipoc debug', time.ctime()
+		print
+		if isinstance(data, str):
+			print data
+		else:
+			pprint(data)
+		print
+		
+if __name__ == '__main__':
+
+	import sys
+	if len(sys.argv) != 3:
+		sys.exit('Usage: __init__.py login haslo')
+	username = sys.argv[1]
+	password = sys.argv[2]
+	u = Update()
+
+	u.set_credentials(username, password)
+	#pprint(u.dashboard())
+	#u.dashboard(140000) # nie działa
+	#sys.exit()
+	#u.get(140000)
+	u.set('how do you handle f')
+
+	u = User()
+	
+	u.set_credentials(username, password)
+	pprint(u.friends())
+	
+	pprint(friends)
+	pprint(u.users())
+	
+
